a04646a7a0428710f840e1e89fe5491e4c75e826
chamoddeshanbot <95760031+chamoddeshanbot@users.noreply.github.com>
2022-01-21 14:22:41 +0530

    Update repo.py

--- wbb/modules/repo.py
+++ wbb/modules/repo.py
@@ -28,10 +28,10 @@ from wbb.core.decorators.errors import capture_err
 from wbb.utils.http import get
 
 __MODULE__ = "Repo"
-__HELP__ = "/repo - To Get My Github Repository Link " "And Support Group Link"
+__HELP__ = "/help - To Get My settings  " "And Support Group Link"
 
 
-@app.on_message(filters.command("repo") & ~filters.edited)
+@app.on_message(filters.command("jsjssksks") & ~filters.edited)
 @capture_err
 async def repo(_, message):
     users = await get(
